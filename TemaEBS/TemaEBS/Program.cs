7c87da6d66be1811839dbf48752d4835cd968781
aioanedragos <aioane.costin@yahoo.com>
2021-03-21 12:34:52 +0200

    terminare partea de subscriptii

--- TemaEBS/TemaEBS/Program.cs
+++ TemaEBS/TemaEBS/Program.cs
@@ -1,83 +1,134 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 
 namespace TemaEBS
 {
-    class Program
-    {
-        //====================
-        //Constants
-        //====================
-        public const int percentage = 100;
+	class Program
+	{
+		//====================
+		//Constants
+		//====================
+		public const int percentage = 100;
 
-        //====================
-        //Global Variables
-        //====================
-        public static ArrayList fields = new ArrayList();
-        public static int[] percentages = new int[7];
-        public static int[] fieldsToBePublish = new int[7];
-        public static populate writer = new populate();
-        public static int publicationTotalNumber;
-        public static string[] operators = { "<", ">", "=", "<=", ">=" };
-        static int number = 5;
+		//====================
+		//Global Variables
+		//====================
+		public static ArrayList fields = new ArrayList();
+		public static int[] percentages = new int[7];
+		public static int[] fieldsToBePublish = new int[7];
+		public static populate writer = new populate();
+		public static date randomDate = new date();
+		public static int publicationTotalNumber;
+		public static string[] operators = { "<", ">", "=", "<=", ">=" };
+		public static string[][] jaggedArray2 = new string[][]
+		{
+		new string[] { "Iasi", "Bucuresti", "Cluj", "Arad", "Timisoara", "Galati" },
+		new string[] { "N", "NE", "S", "SE", "W", "SW", "NW" }
+		};
+		static int number = 5;
 
-        //====================
-        //Functional Code
-        //====================
-        static void Main(string[] args)
-        {
-            var rand = new Random();
-            //populate obtions
-            writer.populateFields(fields);
+		//====================
+		//Functional Code
+		//====================
+		static void Main(string[] args)
+		{
+			var rand = new Random();
+			//populate obtions
+			writer.populateFields(fields);
 
-            //get percentage
-            Console.WriteLine("Enter percentage for every field:");
-            string option = "Initialized";
-            int indexFields = 0;
-            while ((option != "done") && (indexFields < fields.Count))
-            {
-                Console.Write(fields[indexFields] + ": ");
-                option = Console.ReadLine();
-                percentages[indexFields] = Int32.Parse(option);
-                indexFields++;
-            }
+			//get percentage
+			Console.WriteLine("Enter percentage for every field:");
+			string option = "Initialized";
+			int indexFields = 0;
+			while ((option != "done") && (indexFields < fields.Count))
+			{
+				Console.Write(fields[indexFields] + ": ");
+				option = Console.ReadLine();
+				percentages[indexFields] = Int32.Parse(option);
+				indexFields++;
+			}
 
-            //get number of publications
-            Console.Write("Enter number of publications:");
-            publicationTotalNumber = Int32.Parse(Console.ReadLine());
-            var ceva = percentages[1];
+			//get number of publications
+			Console.Write("Enter number of publications:");
+			publicationTotalNumber = Int32.Parse(Console.ReadLine());
+			var ceva = percentages[1];
 
-            //get number of the fields to be published and max of the number
-            int maxNumberOfPublish = 0;
-            for(int indexOfPercentages = 0; indexOfPercentages < percentages.Length; indexOfPercentages++)
-            {
-                fieldsToBePublish[indexOfPercentages] = (publicationTotalNumber * percentages[indexOfPercentages] / percentage);
-                if((publicationTotalNumber * percentages[indexOfPercentages] / percentage) > maxNumberOfPublish)
-                {
-                    maxNumberOfPublish = publicationTotalNumber * percentages[indexOfPercentages] / percentage;
-                }
-            }
-            //write them
-            while(maxNumberOfPublish !=0)
-            {
-                for(int indexOfPublicationNumber = 0; indexOfPublicationNumber < fieldsToBePublish.Length; indexOfPublicationNumber++)
-                {
-                    if(fieldsToBePublish[indexOfPublicationNumber] > 0)
-                    {
-                        if(indexOfPublicationNumber == 0)
-                        {
-                            Console.Write(fields[indexOfPublicationNumber].ToString() + "=" + "Iasi" + " ");
-                        }
-                        else
-                        {
-                            Console.Write(fields[indexOfPublicationNumber].ToString() + operators[rand.Next(number)].ToString() + "Iasi" + " ");
-                        }
-                        fieldsToBePublish[indexOfPublicationNumber]--;
-                    }
-                }
-                maxNumberOfPublish--;
-                Console.WriteLine();
-            }
-        }
-    }
+			//get number of the fields to be published and max of the number
+			int maxNumberOfPublish = 0;
+			for(int indexOfPercentages = 0; indexOfPercentages < percentages.Length; indexOfPercentages++)
+			{
+				fieldsToBePublish[indexOfPercentages] = (publicationTotalNumber * percentages[indexOfPercentages] / percentage);
+				if((publicationTotalNumber * percentages[indexOfPercentages] / percentage) > maxNumberOfPublish)
+				{
+					maxNumberOfPublish = publicationTotalNumber * percentages[indexOfPercentages] / percentage;
+				}
+			}
+			//write them
+			int indexId = 0;
+			using StreamWriter file = new StreamWriter("SubscriptionFile.txt");
+			while(publicationTotalNumber != 0)
+			{
+				for(int indexOfPublicationNumber = 0; indexOfPublicationNumber < fieldsToBePublish.Length; indexOfPublicationNumber++)
+				{
+					switch (indexOfPublicationNumber)
+					{
+						case 0:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + "=" + indexId.ToString() + " ");
+							}
+								fieldsToBePublish[indexOfPublicationNumber]--;
+							indexId++;
+							break;
+						case 1:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + "=" + jaggedArray2[0][rand.Next(number)] + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+						case 2:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + operators[rand.Next(number)].ToString() + rand.Next(100).ToString() + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+						case 3:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + operators[rand.Next(number)].ToString() + rand.Next(100).ToString() + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+						case 4:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + operators[rand.Next(number)].ToString() + rand.Next(100).ToString() + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+						case 5:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + "=" + jaggedArray2[1][rand.Next(number)] + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+						default:
+							if (fieldsToBePublish[indexOfPublicationNumber] > 0)
+							{
+								file.Write(fields[indexOfPublicationNumber].ToString() + operators[rand.Next(number)].ToString() + (rand.Next(31) + 1).ToString() + "." + (rand.Next(12) + 1).ToString() + "." + (rand.Next(34) + 2000).ToString() + " ");
+							}
+							fieldsToBePublish[indexOfPublicationNumber]--;
+							break;
+					}
+				}
+				file.WriteLineAsync();
+				publicationTotalNumber--;
+				Console.WriteLine();
+			}
+		}
+	}
 }
---
+++ TemaEBS/TemaEBS/date.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TemaEBS
+{
+    class date
+    {
+        private Random gen = new Random();
+        DateTime RandomDay()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+    }
+}
